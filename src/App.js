6f295d04a23d9e68339d5f991a008f88c5eef882
Fanylin <fanylin0713@gmail.com>
2021-01-30 19:11:15 +0800

    [client] print star

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  const [upStar, setUpStar] = useState('')
+  const [downStar, setDownStar] = useState('')
+
+  const handlePrintStar1 = () => {
+    const num = document.getElementById('number').value;
+    let starTemp =''
+	  for(var i = 1; i<=num; i++){
+      for(var j = 1; j<=i; j++){
+        starTemp += '*';
+      }
+      starTemp += "\n";
+    }
+    setUpStar(starTemp)
+  };
+
+  const handlePrintStar2 = () => {
+    const num = document.getElementById('number').value;
+    let starTemp =''
+	  for(var i = num-1; i>0; i--){
+      for(var j = 1; j<=i; j++){
+        starTemp += '*';
+      }
+      starTemp += "\n";
+    }
+    setDownStar(starTemp)
+  };
+
+  const print = () => {
+    handlePrintStar1();
+    handlePrintStar2();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input id="number" type="number"  placeholder="請輸入數字1~100：" />
+        <input type="button" value="print" onClick={print}/>
+      </div>
+      <div style={{whiteSpace: 'pre-line'}}>{upStar}</div>
+      <div style={{whiteSpace: 'pre-line'}}>{downStar}</div>
     </div>
   );
 }
